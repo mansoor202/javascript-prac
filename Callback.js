ff75385f017a7500c2e5c6ca25aaf34d4125a1ac
mansoor202 <mansoor.ahmad@focustreck.com>
2021-10-13 07:10:15 -0700

    evaluated Topper and wished him

---
+++ Callback.js
@@ -0,0 +1,41 @@
+let students = {
+    "mansoor": 30,
+    "shabbir": 20,
+    "Subhan": 45
+}
+
+let topper = {}
+function greetings(name, rank) {
+    console.log(`congratulation ${name}: You stood ${rank} `)
+}
+
+function getTopper(students) {
+    return new Promise((resolve, reject) => {
+        
+        let marks = Object.values(students)
+        let maxMArks = Math.max(...marks)
+        console.log("MAX :", maxMArks)
+
+        let stuArray = Object.entries(students)
+        stuArray.forEach(element => {
+            if (element[1] == maxMArks) {
+                topper.name = element[0];
+                topper.rank = "1st"
+                resolve(topper);
+
+            }
+
+
+
+        })
+        reject("couldn't resolve")
+
+
+
+    });
+}
+
+
+getTopper(students).then((res)=>{
+    greetings(res.name,res.rank);
+})
