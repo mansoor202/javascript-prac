fb5eca2a99569afa34495affba553a930333c92d
mansoor202 <mansoor.ahmad@focustreck.com>
2021-10-13 04:44:44 -0700

    no matter the function is resolved at compile time or run time,it accepts the same updated vaklue

--- VariableScope.js
+++ VariableScope.js
@@ -30,3 +30,20 @@ function makeCounter() {
   console.log("First Incremented value by adder1 ",Adder1())
   console.log("Second Incremented value by adder1",Adder1())
   console.log("Incremented value by Adder2",Adder2())
+
+
+  /*************************** */
+
+let userName = "John";
+function sayHi1() {
+    return (userName);
+  }
+
+sayHi2=function() {
+  return (userName);
+}
+
+userName = "Pete";
+
+console.log("Updated name is ", sayHi1());
+// console.log("Updated name is ", sayHi2());
