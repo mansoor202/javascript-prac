11e82b29f1627e37679df798712f3a4ad9238042
mansoor202 <mansoor.ahmad@focustreck.com>
2021-10-13 03:57:41 -0700

    spread for rest parameters

--- spread.js
+++ spread.js
@@ -10,11 +10,37 @@ spreadSingleArray()
 
 function spreadTwoArrays(){
 
+    let max=90;
+    let min=3;
     let class1 = [23,34,54,59,23];
     let class2 = [42,34,30,35,23];
-    console.log( Math.max(...class1,...class2) );
+    console.log( Math.max(max,...class1,...class2,min) );
     }
 
 
 spreadTwoArrays()
+
+
+//receiving rest parameters
+
+function maxAge(age1,age2,...remainings){
+    let max;
+    max=(age1>age2) ? age1:age2;
+    if (remainings.length==0){
+        
+        console.log(`max age is ${max}`);
+        return;
+        
+    }
+    else{
+       
+       maxOfArray=Math.max(...remainings);
+       max=(max>maxOfArray) ? max:maxOfArray;
+        
+    }
     
+    console.log(`max of all given numbers is  ${max}`);
+}
+ 
+
+maxAge(23,12,23,34,29)
