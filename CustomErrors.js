3ee541c65ae4a588c81a59489dd396e20a30d61e
mansoor202 <mansoor.ahmad@focustreck.com>
2021-10-13 06:25:51 -0700

    extended our own error

--- CustomErrors.js
+++ CustomErrors.js
@@ -5,6 +5,8 @@ class ValidateInput extends Error {
     constructor(message) {
       super(message);
       this.name = "ValidationError";
+      this.type="EmptyInputCheck"
+
     }
   }
 
@@ -31,5 +33,35 @@ catch(err){
     console.log("Type: ",err.name)
     console.log("Description: ",err.message)
 }
+
+
+/*********** Further extending my own custom error ******/
+
+class ValidateIndividualInput extends ValidateInput{
+    constructor(message) {
+      super(message);
+      this.name = "Something doesn't according to format";
+      this.type="individual Value checking";
+
+    }
+  }
+
+  try{
+    let user=input2.userName;
+    for (let i=0;i<user.length;i++){
+    let code=user.charCodeAt(i) 
+    if (code >=65 && code <=90 && code >=97 && code <=122){
+        console.log("valid name input")
+    }
+    else{
+        throw new ValidateIndividualInput("only english alphbets are allowed")
+    }
+    }
+    }
+    catch(err){
+        console.log("Type: ",err.name)
+        console.log("Description: ",err.message)
+    }
+
 
 
