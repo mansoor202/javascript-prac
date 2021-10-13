c1a29ff400922c84db7739b827ca8b1a33edd8a3
mansoor202 <mansoor.ahmad@focustreck.com>
2021-10-13 04:26:41 -0700

    variable returned from a function have their local scope

--- VariableScope.js
+++ VariableScope.js
@@ -1,4 +1,4 @@
-/*************Both name variables are scoped to their blocks  */
+/*************  Both name variables are scoped to their blocks  *****/
 
 {
     let name="spaceX";
@@ -12,3 +12,21 @@
 
 }
 
+
+/*************************** */
+
+
+function makeCounter() {
+    let count = 0;
+  
+    return function() {
+      return ++count;
+    };
+  }
+  
+  let Adder1 = makeCounter();
+  let Adder2 = makeCounter();
+
+  console.log("First Incremented value by adder1 ",Adder1())
+  console.log("Second Incremented value by adder1",Adder1())
+  console.log("Incremented value by Adder2",Adder2())
