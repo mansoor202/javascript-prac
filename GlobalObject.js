94f82507cf96a002b337693d545903e9b82903e2
mansoor202 <mansoor.ahmad@focustreck.com>
2021-10-13 04:50:51 -0700

    Global object as a general

---
+++ GlobalObject.js
@@ -0,0 +1,20 @@
+//global object that provides functions and variable that are available everywhere
+
+console.log(globalThis)
+
+// output of the above statement showing what gloablThis is generally include
+
+{/* <ref *1> Object [global] {
+  global: [Circular *1],
+  clearInterval: [Function: clearInterval],
+  clearTimeout: [Function: clearTimeout],
+  setInterval: [Function: setInterval],
+  setTimeout: [Function: setTimeout] {
+    [Symbol(nodejs.util.promisify.custom)]: [Getter]
+  },
+  queueMicrotask: [Function: queueMicrotask],
+  clearImmediate: [Function: clearImmediate],
+  setImmediate: [Function: setImmediate] {
+    [Symbol(nodejs.util.promisify.custom)]: [Getter]
+  }
+} */}
