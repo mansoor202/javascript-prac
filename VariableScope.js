f55e813bc170bd7c0ba57d2e968d3268746507b3
mansoor202 <mansoor.ahmad@focustreck.com>
2021-10-13 04:12:15 -0700

    variables scope to their blocks

---
+++ VariableScope.js
@@ -0,0 +1,14 @@
+/*************Both name variables are scoped to their blocks  */
+
+{
+    let name="spaceX";
+    console.log(`company1: ${name}`)
+
+}
+
+{
+    let name="Microsoft";
+    console.log(`company1: ${name}`)
+
+}
+
