8510e3ddc3d853629bf6550433451eb904cdbb3b
mansoor202 <mansoor.ahmad@focustreck.com>
2021-10-13 03:42:28 -0700

    spreaded two arrays

--- spread.js
+++ spread.js
@@ -1,2 +1,20 @@
-let Marks = [23,34,54,59,23];
-console.log( Math.max(...arr) );
+
+function spreadSingleArray(){
+
+let marks = [23,34,54,59,23];
+console.log( Math.max(...marks) );
+}
+
+spreadSingleArray()
+
+
+function spreadTwoArrays(){
+
+    let class1 = [23,34,54,59,23];
+    let class2 = [42,34,30,35,23];
+    console.log( Math.max(...class1,...class2) );
+    }
+
+
+spreadTwoArrays()
+    
