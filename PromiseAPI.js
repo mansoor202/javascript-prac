aac04624e99e0b72a6c9312f06bc3ec2e46231ab
mansoor202 <mansoor.ahmad@focustreck.com>
2021-10-13 23:19:51 -0700

    Promise.all example

--- PromiseAPI.js
+++ PromiseAPI.js
@@ -1,2 +1,64 @@
-// Hi this is mansoor
-console.log("Promise API generated")
+ let input0="jackbush"
+ let input1="johnson"
+ let input2="Mikal23"
+function checkAlphabetsOnly(input){
+    return new Promise((resolve,reject)=>{
+
+        try{
+            for (let i=0;i<input.length;i++){
+                let code=input.charCodeAt(i)
+                if ((code >=65 && code <=90) || (code >=97 && code <=122)){
+                    continue;
+                }
+                else{
+                    throw new Error("only english alphbets are allowed");
+                }
+        
+            }
+            resolve (input)
+            
+        }
+        catch(err){
+           
+            reject (err)
+        }
+        
+
+    })
+
+  
+}
+
+function checkLength(input,minLength){
+    return new Promise((resolve,reject)=>{
+        try{
+         if(input.length>=minLength){
+             resolve(input)
+         }
+         else{
+             throw new Error(`password should be ${minLength} chars long`)
+         }
+        }
+        catch(err){
+            reject(err.message);
+        }
+
+    })
+
+}
+
+// checkAlphabetsOnly("Tajamal")
+// .then(input=>console.log("valid input :",input))
+// .catch(err=>console.log(err.message))
+
+// checkLength("Tajamal")
+// .then(res=>console.log("Full length password okay! ",res))
+// .catch(err=>console.log(err))
+
+
+Promise.all([
+    checkAlphabetsOnly(input0),
+    checkLength(input0,5)
+])
+.then(res=>console.log("Promise.all resolved"))
+.catch(err=>console.log(err))
